1c7ef2f87337ffa261560040ff9dd0f52f0eab23
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-07 20:57:54 +0200

    trial with secret key

--- astrydastories/settings.py
+++ astrydastories/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3oh97b&e70hjpl8^uue(rn)0!1l253(=b*cq++0nayx!v7t))('
+#SECRET_KEY = 'django-insecure-3oh97b&e70hjpl8^uue(rn)0!1l253(=b*cq++0nayx!v7t))('
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
