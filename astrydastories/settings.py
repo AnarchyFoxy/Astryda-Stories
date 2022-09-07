aa52c1f7d649cf2e5b1722d25cbbde5fd287afdf
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-07 21:15:09 +0200

    Secret Key issue finally solved

--- astrydastories/settings.py
+++ astrydastories/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,6 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-3oh97b&e70hjpl8^uue(rn)0!1l253(=b*cq++0nayx!v7t))('
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
