55744a43e1ea221dd51a2d37d72ac617f54bc873
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-07 21:17:01 +0200

    Secret Key problem solved

--- astrydastories/settings.py
+++ astrydastories/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-3oh97b&e70hjpl8^uue(rn)0!1l253(=b*cq++0nayx!v7t))('
+
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
